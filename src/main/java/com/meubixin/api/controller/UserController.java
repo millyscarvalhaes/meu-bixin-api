7dc78b255bc4289a68a8fad6264efade0f2e1655
Millys Carvalhaes <millysfabrielle@gmail.com>
2022-05-23 16:46:55 -0300

    Created user controller.

---
+++ src/main/java/com/meubixin/api/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.meubixin.api.controller;
+
+import com.meubixin.api.entity.User;
+import com.meubixin.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public User getUserDetails(Principal user){
+        return Optional.ofNullable(userRepository.findByEmail(user.getName()))
+                .map( optional -> optional.get())
+                .orElseThrow( () -> new UsernameNotFoundException("User not found!"));
+    }
+
+}
