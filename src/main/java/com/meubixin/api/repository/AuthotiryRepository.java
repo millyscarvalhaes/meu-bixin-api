af215afe6ac5097e93c93367327df2c0852ae978
Millys Carvalhaes <millysfabrielle@gmail.com>
2022-05-23 16:33:41 -0300

    created repositories

---
+++ src/main/java/com/meubixin/api/repository/AuthotiryRepository.java
@@ -0,0 +1,7 @@
+package com.meubixin.api.repository;
+
+import com.meubixin.api.entity.Authority;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthotiryRepository extends CrudRepository<Authority, Long> {
+}
---
+++ src/main/java/com/meubixin/api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.meubixin.api.repository;
+
+import com.meubixin.api.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
