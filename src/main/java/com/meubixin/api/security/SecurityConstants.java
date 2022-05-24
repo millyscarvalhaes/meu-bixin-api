31cc91b48b6a29c4a9fb61e7279772fc5c7b6f18
Millys Carvalhaes <millysfabrielle@gmail.com>
2022-05-24 13:34:49 -0300

    created security constant file

--- src/main/java/com/meubixin/api/security/SecurityConstants.java
+++ src/main/java/com/meubixin/api/security/SecurityConstants.java
@@ -1,13 +1,20 @@
 package com.meubixin.api.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SecurityConstants {
 
-    @Value("${security.JWT_SECRET:jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4}")
     public static String JWT_SECRET;
 
-    @Value("${security.JWT_HEADER:Authorization")
     public static String JWT_HEADER;
 
+    public SecurityConstants(
+            @Value("${app.security.jwt-secret:jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4}") String jwtSecret,
+            @Value("${app.security.jwt-header:Authorization}") String jwtHeader
+    ) {
+        SecurityConstants.JWT_SECRET = jwtSecret;
+        SecurityConstants.JWT_HEADER = jwtHeader;
+    }
 }
